d92f843ffdc87038215c982800fe9aa524ad2f7c
student <student@DESKTOP-5577H91>
2017-11-09 12:01:14 +0100

    Inicijalni upload

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -28,12 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridViewCountries = new System.Windows.Forms.DataGridView();
+            this.comboBoxRegion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxSort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCountries)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCountries
+            // 
+            this.dataGridViewCountries.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCountries.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridViewCountries.Location = new System.Drawing.Point(182, 23);
+            this.dataGridViewCountries.Name = "dataGridViewCountries";
+            this.dataGridViewCountries.Size = new System.Drawing.Size(643, 311);
+            this.dataGridViewCountries.TabIndex = 0;
+            // 
+            // comboBoxRegion
+            // 
+            this.comboBoxRegion.FormattingEnabled = true;
+            this.comboBoxRegion.Location = new System.Drawing.Point(6, 23);
+            this.comboBoxRegion.Name = "comboBoxRegion";
+            this.comboBoxRegion.Size = new System.Drawing.Size(158, 21);
+            this.comboBoxRegion.TabIndex = 1;
+            this.comboBoxRegion.SelectedIndexChanged += new System.EventHandler(this.comboBoxRegion_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kontinenti";
+            // 
+            // comboBoxSort
+            // 
+            this.comboBoxSort.FormattingEnabled = true;
+            this.comboBoxSort.Location = new System.Drawing.Point(6, 78);
+            this.comboBoxSort.Name = "comboBoxSort";
+            this.comboBoxSort.Size = new System.Drawing.Size(158, 21);
+            this.comboBoxSort.TabIndex = 3;
+            this.comboBoxSort.SelectedIndexChanged += new System.EventHandler(this.comboBoxSort_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sortiraj po";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Pretraži";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Pretraži";
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "sCode";
+            this.Column1.HeaderText = "Code";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "sName";
+            this.Column2.HeaderText = "Naziv";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "sCapital";
+            this.Column3.HeaderText = "Glavni grad";
+            this.Column3.Name = "Column3";
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "nPopulation";
+            this.Column5.HeaderText = "Broj stanovnika";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "fArea";
+            this.Column6.HeaderText = "Površina";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "sRegion";
+            this.Column7.HeaderText = "Kontinent";
+            this.Column7.Name = "Column7";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1004, 379);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.dataGridViewCountries);
+            this.tabPage1.Controls.Add(this.comboBoxRegion);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.comboBoxSort);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(996, 353);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Pregled";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(996, 353);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Unos";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.webBrowser1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(996, 353);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Karta svijeta";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(996, 353);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://www.openstreetmap.org", System.UriKind.Absolute);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1039, 403);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "REST Countries";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCountries)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCountries;
+        private System.Windows.Forms.ComboBox comboBoxRegion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxSort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +16,127 @@ namespace WindowsFormsApp
 {
     public partial class Form1 : Form
     {
+        public List<Country> lCountries;
         public Form1()
         {
             InitializeComponent();
+            /*DATA GRID*/
+            lCountries = GetCountries();
+            dataGridViewCountries.DataSource = lCountries;
+            /*COMBO BOX filtriranje*/
+            List<string> lRegions = lCountries.Where(o => o.sRegion != "").Select(o => o.sRegion).Distinct().ToList();
+            lRegions.Insert(0, "Svi kontinenti");
+            comboBoxRegion.DataSource = lRegions;
+            /*COMBO BOX sortiranje*/
+            List<string> lSortCriterias = new List<string>()
+            {
+                "‐",
+                "Glavni grad",
+                "Naziv",
+                "Broj stanovnika",
+                "Povrsina"
+            };
+            comboBoxSort.DataSource = lSortCriterias;
+        }
+        public List<Country> GetCountries()
+        {
+            List<Country> lRESTCountries = new List<Country>();
+            string sUrl = System.Configuration.ConfigurationManager.AppSettings["RestApiUrl"];
+            string sJson = CallRestMethod(sUrl);
+
+            JArray json = JArray.Parse(sJson);
+            foreach (JObject item in json)
+            {
+                //ČITANJE VRIJEDNOSTI IZ JSON-a
+                string code = (string)item.GetValue("alpha2Code");
+                string name = (string)item.GetValue("name");
+                string capital = (string)item.GetValue("capital");
+                int population = (int)item.GetValue("population");
+                //float area = (float)item.GetValue("area");
+                float area = -1;
+                if (item.GetValue("area").Type == JTokenType.Null)
+                {
+                    area = 0;
+                }
+                else
+                {
+                    area = (float)item.GetValue("area");
+                }
+                string region = (string)item.GetValue("region");
+                //DODAVANJE OBJEKATA U LISTU
+                lRESTCountries.Add(new Country
+                {
+                    sCode = code,
+                    sName = name,
+                    sCapital = capital,
+                    nPopulation = population,
+                    fArea = area,
+                    sRegion = region
+                });
+            }
+            return lRESTCountries;
+        }
+
+        public static string CallRestMethod(string url)
+        {
+            HttpWebRequest webrequest = (HttpWebRequest)WebRequest.Create(url);
+            webrequest.Method = "GET";
+            webrequest.ContentType = "application/x-www-form-urlencoded"; 
+            //webrequest.Headers.Add("Username", "xyz");
+            //webrequest.Headers.Add("Password", "abc");
+            HttpWebResponse webresponse = (HttpWebResponse)webrequest.GetResponse();
+            Encoding enc = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader responseStream = new StreamReader(webresponse.GetResponseStream(),
+            enc);
+            string result = string.Empty;
+            result = responseStream.ReadToEnd();
+            webresponse.Close();
+            return result;
+        }
+
+        private void comboBoxRegion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //citanje odabrane vrijednosti
+            string sRegion = (string)comboBoxRegion.SelectedItem;
+
+            lCountries = GetCountries();
+            if(sRegion != "Svi kontinenti")
+            {
+                lCountries = lCountries.Where(o => o.sRegion == sRegion).ToList();
+                dataGridViewCountries.DataSource = lCountries;
+            }
+            else
+            {
+                dataGridViewCountries.DataSource = lCountries;
+            }
+        }
+
+        private void comboBoxSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sCriteria = (string)comboBoxSort.SelectedItem;
+            switch(sCriteria)
+            {
+                case "-":
+                    break;
+                case "Glavni grad":
+                    dataGridViewCountries.DataSource = lCountries.OrderBy(o => o.sCapital).ToList();
+                    break;
+                case "Naziv":
+                    dataGridViewCountries.DataSource = lCountries.OrderBy(o => o.sName).ToList();
+                    break;
+                case "Broj stanovnika":
+                    dataGridViewCountries.DataSource = lCountries.OrderByDescending(o => o.nPopulation).ToList();
+                    break;
+                case "Povrsina":
+                    dataGridViewCountries.DataSource = lCountries.OrderByDescending(o => o.fArea).ToList();
+                    break;
+
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
